865c37c1ce6bb95915a0d3513d1f1657f3b95ea4
Antonnio Jones <antonnioj@gmail.com>
2020-08-09 15:14:03 -0400

    Refactoring & Organising code

---
+++ functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apiKey: "AIzaSyCt2ELKmfn1dmh0hGdF1takGnOg2Vnr6v4",
+  authDomain: "social-media-a1b23.firebaseapp.com",
+  databaseURL: "https://social-media-a1b23.firebaseio.com",
+  projectId: "social-media-a1b23",
+  storageBucket: "social-media-a1b23.appspot.com",
+  messagingSenderId: "737524978788",
+  appId: "1:737524978788:web:6b239ec2edd91eb810bb71",
+  measurementId: "G-MXE5QLHMH1",
+};
---
+++ functions/util/validators.js
@@ -0,0 +1,65 @@
+//check for empty string
+const isEmpty = (string) => {
+  if (string.trim() === "") {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+//checks valiate email
+const isEmail = (email) => {
+  const emailRegEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (email.match(emailRegEx)) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+exports.validateSignupData = (data) => {
+  let errors = {};
+
+  //validate email
+  if (isEmpty(data.email)) {
+    errors.email = "Email must not be empty";
+  } else if (!isEmail(data.email)) {
+    errors.email = "Must be a valid email address";
+  }
+
+  //validate password
+  if (isEmpty(data.password)) {
+    errors.password = "password must not be empty";
+  }
+
+  //validate confirm password
+  if (data.password !== data.confirmPassword) {
+    errors.confirmPassword = "passwords must match";
+  }
+
+  //validate handle
+  if (isEmpty(data.handle)) {
+    errors.handle = "handle must not be empty";
+  }
+
+  return {
+    errors,
+    valid: Object.keys(errors).length === 0 ? true : false,
+  };
+};
+
+exports.validateLoginData = (data) => {
+  //validation
+  let errors = {};
+  if (isEmpty(data.email)) {
+    errors.email = "Email Must not be empty";
+  }
+  if (isEmpty(data.password)) {
+    errors.password = "password must not be empty";
+  }
+
+  return {
+    errors,
+    valid: Object.keys(errors).length === 0 ? true : false,
+  };
+};
