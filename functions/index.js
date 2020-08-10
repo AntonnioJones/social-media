0fdfe776f303c6e09e089d00b2fd96759d4e7961
Antonnio Jones <antonnioj@gmail.com>
2020-08-10 14:43:35 -0400

    added image uploading

--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,7 @@ const functions = require("firebase-functions");
 const app = require("express")();
 const FBAuth = require("./util/FBAuth");
 const { getAllScreams, postOneScream } = require("./handlers/screams");
-const { signup, login } = require("./handlers/users");
+const { signup, login, uploadImage } = require("./handlers/users");
 
 //screams route
 app.get("/screams", getAllScreams);
@@ -11,5 +11,8 @@ app.post("/scream", FBAuth, postOneScream);
 //users routes
 app.post("/signup", signup);
 app.post("/login", login);
+
+//upload images 
+app.post("/user/image", FBAuth , uploadImage);
 
 exports.api = functions.region("us-east1").https.onRequest(app);
