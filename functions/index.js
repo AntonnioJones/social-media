6b18215ac04251d5ed17718e7d98965914ec968f
Antonnio Jones <antonnioj@gmail.com>
2020-08-13 12:10:43 -0400

    more handleres

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,21 @@
 const functions = require("firebase-functions");
 const app = require("express")();
 const FBAuth = require("./util/FBAuth");
-const { getAllScreams, postOneScream } = require("./handlers/screams");
+const { getAllScreams, postOneScream, getScream } = require("./handlers/screams");
 const { signup, login, uploadImage, addUserDetails, getAuthenticatedUser } = require("./handlers/users");
 
 //screams route
 app.get("/screams", getAllScreams); //get all screams
-app.post("/scream", FBAuth, postOneScream); //get one scream
-app.post("/user/image", FBAuth , uploadImage); //upload images 
-app.get('/user',FBAuth, getAuthenticatedUser)
+app.post("/scream", FBAuth, postOneScream); //post one scream
+app.get('/scream/:screamId', getScream);  //get one scream
+
 
 //users routes
 app.post("/signup", signup);//sign up
 app.post("/login", login);//login
+app.post("/user/image", FBAuth , uploadImage); //upload images 
 app.post('/user', FBAuth, addUserDetails)//add details to user profile
+app.get('/user',FBAuth, getAuthenticatedUser)
 
 
 exports.api = functions.region("us-east1").https.onRequest(app);
